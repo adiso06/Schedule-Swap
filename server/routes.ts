9a7c9271b1c78d066edab76e5afd15af73a61fa8
Aditya Sood <aditya.sood2@gmail.com>
2025-05-05 05:39:58 -0400

    .

--- server/routes.ts
+++
@@ -1,8 +0,0 @@
-import type { Express } from "express";
-import { createServer, type Server } from "http";
-
-// Create and return a basic HTTP server
-export async function registerRoutes(app: Express): Promise<Server> {
-  const httpServer = createServer(app);
-  return httpServer;
-} 
